130c19db0cb10941858137c8a821a3ffd4220103
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-03-09 16:50:43 -0500

    Add project files.

---
+++ HFLambdaConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HFLambdaConsole
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        static double GetRandomDouble(int max) => max * random.NextDouble();
+        
+        static void PrintValue(double d) => Console.WriteLine($"The value is {d:0.000}");
+     
+
+
+        static void Main(string[] args)
+        {
+            var value = Program.GetRandomDouble(100);
+            Program.PrintValue(value);
+
+
+        }
+    }
+}
